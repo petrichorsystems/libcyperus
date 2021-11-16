5eccaaa54729f8d7b8ee15eec713282068ba762b
resinbeard <mrafoster@gmail.com>
2021-11-15 22:42:49 -0800

    added lua test script so i don't have to keep typing this in over and over

---
+++ src/lua/test/test.lua
@@ -0,0 +1,22 @@
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+
+require('Cyperus')
+local cyperus
+cyperus = Cyperus.new('5000', '127.0.0.1', '5001')
+ins = cyperus.get_ins()
+outs = cyperus.get_outs()
+
+print(dump(ins))
+print(dump(outs))
